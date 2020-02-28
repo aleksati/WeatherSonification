ce68ea2161f4cfec5285e261b827a74243567837
alexfurimmer <alexfurimmer@eduroam-193-157-215-12.wlan.uio.no>
2020-02-28 13:58:09 +0100

    A few steps further to averaging rgb-values for different sections on the screen.

--- Python/RGB-from-screengrab.py
+++ Python/RGB-from-screengrab.py
@@ -8,29 +8,59 @@ root = tk.Tk()
 screen_width = root.winfo_screenwidth() - 1
 screen_height = root.winfo_screenheight() - 1
 
+inputdimx = 300
+inputdimy = 300
+
 while True: #gets the previous frames rgbavg values.
-    red = []
-    green = []
-    blue = []
-    frame_babel = ImageGrab.grab()
+    red1 = []
+    green1 = []
+    blue1 = []
+    frame_babel = ImageGrab.grab(bbox=(0, 0, inputdimx, inputdimy))
     frame_array = np.array(frame_babel)
     frame = cv2.cvtColor(frame_array, cv2.COLOR_BGR2RGB)
  
-    for y in range(0, screen_height):
-        for x in range(0, screen_width):
-            rgb = frame[y, x]
-            red.append(rgb[0])
-            green.append(rgb[1])
-            blue.append(rgb[2])
+    for y1 in range(0, inputdimy): #First first third of the height
+        for x1 in range(0, (inputdimx/3 * 1)): #Second first third of the width
+            rgb1 = frame[y1, x1]
+            red1.append(rgb1[0])
+            green1.append(rgb1[1])
+            blue1.append(rgb1[2])
 
-    redavg = int(utils.average(red))
-    greenavg = int(utils.average(green))
-    blueavg = int(utils.average(blue))
-    rgbavg = [redavg, greenavg, blueavg]
-    if rgbavg == 
+    redavg1 = int(utils.average(red1))
+    greenavg1 = int(utils.average(green1))
+    blueavg1 = int(utils.average(blue1))
+    rgbavg1 = [redavg1, greenavg1, blueavg1]
 
-    print()
+    for y1 in range(0, inputdimy): #Second first third of the height.
+        for x2 in range(((inputdimx/3)*1), (inputdimx/3 * 2)): #Second first third of the width
+            rgb2 = frame[y1, x2]
+            red2.append(rgb2[0])
+            green2.append(rgb2[1])
+            blue2.append(rgb2[2])
 
+    redavg2 = int(utils.average(red2))
+    greenavg2 = int(utils.average(green2))
+    blueavg2 = int(utils.average(blue2))
+    rgbavg2 = [redavg2, greenavg2, blueavg2]
+
+    for y1 in range(0, inputdimy): #Second first third of the height.
+        for x3 in range(((inputdimx/3) * 2), (inputdimx/3 * 3)): #Second first third of the width
+            rgb3 = frame[y3, x3]
+            red3.append(rgb3[0])
+            green3.append(rgb3[1])
+            blue3.append(rgb3[2])
+
+    redavg3 = int(utils.average(red3))
+    greenavg3 = int(utils.average(green3))
+    blueavg3 = int(utils.average(blue3))
+    rgbavg3 = [redavg3, greenavg3, blueavg3]
+
+    section = []
+    section.append(rgbavg1)
+    section.append(rgbavg2)
+    section.append(rgbavg3)
+    
+    print(section)
 
 
 # Gjennomsnitts verdier i soner. 
