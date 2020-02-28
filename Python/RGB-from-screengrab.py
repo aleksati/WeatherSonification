eef6729079804076ba5a13223695549be85c8aa9
alexfurimmer <alexfurimmer@Aleksanders-MacBook-Pro.local>
2020-02-28 22:21:44 +0100

    little more work on the rgb screen grabbing.

--- Python/RGB-from-screengrab.py
+++ Python/RGB-from-screengrab.py
@@ -16,65 +16,13 @@ while True:
     frame_array = np.array(frame_babel)
     frame = cv2.cvtColor(frame_array, cv2.COLOR_BGR2RGB)
 
-    red1 = []
-    green1 = []
-    blue1 = []
-    red2 = []
-    green2 = []
-    blue2 = []
-    red3 = []
-    green3 = []
-    blue3 = []
-    for y1 in range(0, inputdimy): #First first third of the height
-        for x1 in range(0, round((inputdimx/3 * 1))): #Second first third of the width
-            rgb1 = frame[y1, x1]
-            red1.append(rgb1[0])
-            green1.append(rgb1[1])
-            blue1.append(rgb1[2])
+    first_row_rgb_avg = utils.screensplit_rgb_avg(frame, 1)
+    second_row_rgb_avg = utils.screensplit_rgb_avg(frame, 2)
+    third_row_rgb_avg = utils.screensplit_rgb_avg(frame, 3)
 
-    redavg1 = int(utils.average(red1))
-    greenavg1 = int(utils.average(green1))
-    blueavg1 = int(utils.average(blue1))
-    rgbavg1 = [redavg1, greenavg1, blueavg1]
+    screen_rgb_avg_array = np.array((first_row_rgb_avg), (second_row_rgb_avg), (third_row_rgb_avg))
 
-    for y1 in range(0, inputdimy): #Second first third of the height.
-        for x2 in range(((inputdimx/3)*1), (inputdimx/3 * 2)): #Second first third of the width
-            rgb2 = frame[y1, x2]
-            red2.append(rgb2[0])
-            green2.append(rgb2[1])
-            blue2.append(rgb2[2])
+    print(screen_rgb_avg_array)
 
-    redavg2 = int(utils.average(red2))
-    greenavg2 = int(utils.average(green2))
-    blueavg2 = int(utils.average(blue2))
-    rgbavg2 = [redavg2, greenavg2, blueavg2]
-
-    for y1 in range(0, inputdimy): #Second first third of the height.
-        for x3 in range(((inputdimx/3) * 2), (inputdimx/3 * 3)): #Second first third of the width
-            rgb3 = frame[y1, x3]
-            red3.append(rgb3[0])
-            green3.append(rgb3[1])
-            blue3.append(rgb3[2])
-
-    redavg3 = int(utils.average(red3))
-    greenavg3 = int(utils.average(green3))
-    blueavg3 = int(utils.average(blue3))
-    rgbavg3 = [redavg3, greenavg3, blueavg3]
-
-    section = []
-    section.append(rgbavg1)
-    section.append(rgbavg2)
-    section.append(rgbavg3)
-
-
-
-# Gjennomsnitts verdier i soner. 
-
-#if frame[y, x].all() == frame[y, x-1].all():  #Remove consecutive duplicate indexes.
-
-
-#print (f'red = {red}  green = {green}  blue = {blue}')
-
-
-# Caculate average RGB from each ImageGrab
-#histogram
+# If frame[y, x].all() == frame[y, x-1].all():  #Remove consecutive duplicate indexes.
+# Histograms based on avg. 
--- Python/utils.py
+++ Python/utils.py
@@ -10,10 +10,9 @@ def average(rgblist):
     frame_color_average = int(round((sum(rgblist)) / len(rgblist)))
     return frame_color_average
 
-# 1 should be =
-# y or x 
 
 def screensplit_rgb_avg(ndarray, row_section_number): #1st argument = ndarray , 2nd arguement = select which sections of rows to iterate on. 
+
     source = ndarray
     dimscale = row_section_number
     source.shape[0]
@@ -27,44 +26,44 @@ def screensplit_rgb_avg(ndarray, row_section_number): #1st argument = ndarray , 
     red3 = []
     green3 = []
     blue3 = []
+    section = []
     
     for y1 in range(round(int(((source.shape[1]) / 3) * (dimscale - 1))), round(int(((source.shape[1]) / 3) * dimscale)) ): #First first third of the height
         for x1 in range(0, round(int((source.shape[0]) / 3))): #Second first third of the width
-            rgb1 = frame[y1, x1]
+            rgb1 = source[y1, x1]
             red1.append(rgb1[0])
             green1.append(rgb1[1])
             blue1.append(rgb1[2])
 
-    redavg1 = int(utils.average(red1))
-    greenavg1 = int(utils.average(green1))
-    blueavg1 = int(utils.average(blue1))
+    redavg1 = int(average(red1))
+    greenavg1 = int(average(green1))
+    blueavg1 = int(average(blue1))
     rgbavg1 = [redavg1, greenavg1, blueavg1]
 
     for y1 in range(round(int(((source.shape[1]) / 3) * (dimscale - 1))), round(int(((source.shape[1]) / 3) * dimscale)) ): #Second first third of the height.
-        for x2 in range(round(int(((source.shape[0]) / 3), round(int(((source.shape[0]) / 3) * 2)))) ): #Second first third of the width
-            rgb2 = frame[y1, x2]
+        for x2 in range(round(int(source.shape[0]) / 3), round(int((source.shape[0]) / 3) * 2 )): #Second first third of the width
+            rgb2 = source[y1, x2]
             red2.append(rgb2[0])
             green2.append(rgb2[1])
             blue2.append(rgb2[2])
 
-    redavg2 = int(utils.average(red2))
-    greenavg2 = int(utils.average(green2))
-    blueavg2 = int(utils.average(blue2))
+    redavg2 = int(average(red2))
+    greenavg2 = int(average(green2))
+    blueavg2 = int(average(blue2))
     rgbavg2 = [redavg2, greenavg2, blueavg2]
 
     for y1 in range(round(int(((source.shape[1]) / 3) * (dimscale - 1))), round(int(((source.shape[1]) / 3) * dimscale)) ): #Second first third of the height.
-        for x2 in range(round(int(((source.shape[0]) / 3) * 2), round(int(((source.shape[0]) / 3) * 3))) ): #Second first third of the width
-            rgb3 = frame[y1, x3]
+        for x3 in range(round((int(source.shape[0]) / 3) * 2), round(int((source.shape[0]) / 3) * 3 )): #Second first third of the width
+            rgb3 = source[y1, x3]
             red3.append(rgb3[0])
             green3.append(rgb3[1])
             blue3.append(rgb3[2])
 
-    redavg3 = int(utils.average(red3))
-    greenavg3 = int(utils.average(green3))
-    blueavg3 = int(utils.average(blue3))
+    redavg3 = int(average(red3))
+    greenavg3 = int(average(green3))
+    blueavg3 = int(average(blue3))
     rgbavg3 = [redavg3, greenavg3, blueavg3]
 
-    section = []
     section.append(rgbavg1)
     section.append(rgbavg2)
     section.append(rgbavg3)
