c3fa5ccffa223ed93c7d31287882a4db776b921b
alexfurimmer <alexfurimmer@Aleksanders-MacBook-Pro.local>
2020-02-29 14:32:20 +0100

    some tweaks to RGB-from_screengrab.

--- Python/RGB-from-screengrab.py
+++ Python/RGB-from-screengrab.py
@@ -11,18 +11,27 @@ inputdimy = 300
 while True:
     frame_babel = ImageGrab.grab()
     frame_array = np.array(frame_babel)
-    frame = frame_array[:,:,:3] #Removes Alpha from RGBA
+    frame = frame_array[:,:,:3] #Removes Alpha from RGBA frame array
     #print(frame[0])
     #break
 
-    #first_row_rgb_avg = utils.screensplit_rgb_avg(frame, 1)
+    first_row_rgb_avg = utils.screensplit_rgb_avg(frame, 1)
+    first_row_color_temp = utils.rgb2colortemp(first_row_rgb_avg)
     #print(f'rgb-avg row 1 = {first_row_rgb_avg}')
-    #print(f'colortemp = {utils.rgb2colortemp(first_row_rgb_avg)}')
+    #print(f'colortemp row 1= {first_row_color_temp}')
 
     second_row_rgb_avg = utils.screensplit_rgb_avg(frame, 2)
-    print(f'rgb-avg row 2 = {second_row_rgb_avg}')
-    print(f'colortemp = {utils.rgb2colortemp(second_row_rgb_avg)}')
+    second_row_color_temp = utils.rgb2colortemp(second_row_rgb_avg)
+    #print(f'rgb-avg row 2 = {second_row_rgb_avg}')
+    #print(f'colortemp row 2 = {second_row_color_temp}')
 
-    #third_row_rgb_avg = utils.screensplit_rgb_avg(frame, 3)
+    third_row_rgb_avg = utils.screensplit_rgb_avg(frame, 3)
+    third_row_color_temp = utils.rgb2colortemp(third_row_rgb_avg)
     #print(f'rgb-avg row 3 = {third_row_rgb_avg}')
-    #print(f'colortemp = {utils.rgb2colortemp(third_row_rgb_avg)}')
+    #print(f'colortemp row 3 = {third_row_color_temp}')
+
+    color_temp_array = np.array((first_row_color_temp, second_row_color_temp, third_row_color_temp))
+    print(color_temp_array)
+
+
+
